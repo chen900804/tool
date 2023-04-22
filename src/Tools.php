cbe4284e69f012ae9878c165331df23a0dfcd077
零时差 <476857607@qq.com>
2023-04-22 16:42:52 +0800

    composer依赖配置

--- src/Tools.php
+++ src/Tools.php
@@ -16,8 +16,8 @@ class Tools
      */
     public function __construct()
     {
-        if (!(new Filesystem())->isFile(__DIR__ . '/../config/ZvnTools.php')) {
-            throw new InvalidToolsException('配置文件不存在',401);
+        if (!(new Filesystem())->isFile(dirname(__DIR__) . '/config/ZvnTools.php')) {
+            throw new InvalidToolsException('配置文件不存在', 401);
         }
         $this->config = new Config();
         $this->config->loadConfigFiles(__DIR__ . '/../config');
--- tests/ToolsTest.php
+++ tests/ToolsTest.php
@@ -7,6 +7,6 @@ class ToolsTest extends TestCase
     public function testP()
     {
         $a = new Zvn\Tools\Tools();
-        self::assertIsString("0",$a->config->has('ZvnTools.appid'));
+        self::assertIsString(2,$a->config->get('ZvnTools.appid'));
     }
 }
